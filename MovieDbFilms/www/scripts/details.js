dbec6cfa1cc20b46af88c7f1db2d31233ef51b88
jeremy orion <jeremy9315@hotmail.fr>
2018-01-18 21:41:00 +0100

    ajout page details bdd OK

---
+++ MovieDbFilms/www/scripts/details.js
@@ -0,0 +1,162 @@
+﻿// Pour obtenir une présentation du modèle Vide, consultez la documentation suivante :
+// http://go.microsoft.com/fwlink/?LinkID=397704
+// Pour déboguer du code durant le chargement d'une page dans cordova-simulate ou sur les appareils/émulateurs Android, lancez votre application, définissez des points d'arrêt, 
+// puis exécutez "window.location.reload()" dans la console JavaScript.
+var id = getUrlVars()["id"];
+console.log("id= ", id);
+var movieId = new Array(),
+    sex,
+
+    // 2. This code loads the IFrame Player API code asynchronously.
+    player,
+    tag = document.createElement('script'),
+    firstScriptTag = document.getElementsByTagName('script')[0],
+    done = false,
+    listeVideo = new Array(),
+    $Retour = $('#btnRetour');
+function getUrlVars() {
+    var vars = [], hash; // Déclaration d'un tableau vars et d’une variable "hash"
+
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&'); /* split des paramètres après le "?" de l'url ; les chaînes de
+        caractères possèdent une méthode split() qui permet de les découper en un tableau,
+        en fonction d'un séparateur précisé.*/
+    console.log("les hashes", hashes);
+    console.log('le hashes length = ', hashes.length);
+    for (var i = 0; i < hashes.length; i++) // boucle sur le nombre de paramètres
+    {
+        console.log('le hash brut ', hashes);
+        hash = hashes[i].split('='); // split des paires nom et valeur du paramètre
+        console.log('le hash = ', hash);
+        console.log("le hash 0 ", hash[0]);
+        console.log('le hash 1: ', hash[1]);
+        vars.push(hash[0]);
+        /* La méthode push() permet d'ajouter un ou plusieurs items à un tableau,
+        push() peut recevoir un nombre illimité de paramètres, et chaque paramètre
+        représente un item à ajouter à la fin du tableau.
+        La méthode unshift() fonctionne comme push(), excepté que les items sont ajoutés
+        au début du tableau. Cette méthode n'est pas très fréquente mais peut être utile.
+        Les méthodes shift() et pop() retirent respectivement le premier et le dernier
+        élément du tableau. */
+        vars[hash[0]] = hash[1]; // insertion des valeurs dans le tableau
+    }
+    return vars; // retourne le tableau de paramètres
+};
+
+$Retour.click(function () {
+    window.location = "index.html";
+});
+(function () {
+    "use strict";
+
+    document.addEventListener('deviceready', onDeviceReady.bind(this), false);
+
+    function onDeviceReady() {
+        // Gérer les événements de suspension et de reprise Cordova
+        document.addEventListener('pause', onPause.bind(this), false);
+        document.addEventListener('resume', onResume.bind(this), false);
+    };
+
+    function onPause() {
+        // TODO: cette application a été suspendue. Enregistrez l'état de l'application ici.
+    };
+
+    function onResume() {
+        // TODO: cette application a été réactivée. Restaurez l'état de l'application ici.
+    };
+})();
+tag.src = "https://www.youtube.com/iframe_api";
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+function onYouTubeIframeAPIReady(id) {
+    player = new YT.Player('player', {
+        height: '360',
+        width: '640',
+        videoId: id,
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+}
+
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+    event.target.playVideo();
+}
+
+// 5. The API calls this function when the player's state changes.
+//    The function indicates that when playing a video (state=1),
+//    the player should play for six seconds and then stop.
+function onPlayerStateChange(event) {
+    if (event.data === YT.PlayerState.PLAYING && !done) {
+        setTimeout(stopVideo, 6000);
+        done = true;
+    }
+}
+function stopVideo() {
+    player.stopVideo();
+}
+(function infoFilm() {
+    $.ajax({
+
+        url: "https://api.themoviedb.org/3/movie/" + id + "?api_key=85a1114cc9bcee8c748abdaaade8169a&language=fr-FR",
+        cache: false,
+        type: "GET",
+        dataType: "json",
+        success: function (data) {
+            $('#listFilm').append("<p class='panel'>" + data.original_title + "</p><img class='col-10' src='https://image.tmdb.org/t/p/w500" + data.poster_path + "' alt=" + data.original_title + " /><h2 > Synopsis : </br><span >" + data.overview + "</span></h2><p style='font-size:0.5em'> Sortie :  " + data.release_date + "</p>");
+
+        }
+    });
+    $.ajax({
+
+        url: "https://api.themoviedb.org/3/movie/" + id + "/credits?api_key=85a1114cc9bcee8c748abdaaade8169a",
+        cache: false,
+        type: "GET",
+        dataType: "json",
+        success: function (data) {
+            for (var i = 0; i < data.cast.length; i++) {
+                if (data.cast[i]['gender'] === 0) {
+                    sex = "Non défini";
+                }
+                else if (data.cast[i]['gender'] === 1) {
+                    sex = "Femme";
+                } else sex = "Homme";
+                console.log("les acteurs  :", data.cast[i]['character']);
+                $('#persos').append("<div id='detailsCasting' ><p > Personnage : " + data.cast[i]['character'] + "</br> sexe: " + sex + "</p><p>" + data.cast[i]['name'] + "</p><img class='col-8' src='https://image.tmdb.org/t/p/w500/" + data.cast[i]['profile_path'] + "' alt=" + data.cast[i]['charactere'] + " /><hr /></div>");
+
+            }
+
+        }
+    });
+})();
+$('#btnCast').click(function () {
+    if ($('#detailsCasting').css('display') === 'none') {
+
+        $('#detailsCasting').css('display', 'block');
+    }
+});
+(function getMovies() {
+    $.ajax({
+
+        url: "https://api.themoviedb.org/3/movie/" + id + "/videos?api_key=85a1114cc9bcee8c748abdaaade8169a&language=en-US",
+        cache: false,
+        type: "GET",
+        dataType: "json",
+        success: function (data) {
+            console.log("les data des videos ::", data);
+            movieId.push(data.results);
+            console.log("la longueur des results  ::", data.results.length);
+            //onYouTubeIframeAPIReady(data.results[i]['key']);
+            for (var i = 0; i < data.results.length + 1; i++) {
+                var idVid = data.results[i].key;
+                $('#player').append('<iframe id="player" src= "http://www.youtube.com/embed/' + data.results[i].key + '?enablejsapi=1" frameborder= "0" ></iframe >');
+            }
+        }
+    });
+})();
+
+
+
--- MovieDbFilms/www/scripts/index.js
+++ MovieDbFilms/www/scripts/index.js
@@ -10,81 +10,42 @@ var listePop = [], listeActeur = [];
     document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
 
     function onDeviceReady() {
-        //chargePop();
         // Gérer les événements de suspension et de reprise Cordova
         document.addEventListener( 'pause', onPause.bind( this ), false );
         document.addEventListener( 'resume', onResume.bind( this ), false );
         db.transaction(creerTableFilms, onError, onSuccess);
         db.transaction(creerTableActeurs, onError, onSuccess);
         chargePop();
-        //db.transaction(save2Db, onError, onSuccess);
-        save2Db();
-        console.log("lecture taille avant timer:"+listePop.length);
-        setInterval(function () {
-            console.log("lecture taille hors ajax:" + listePop.length);
-        }, 5000);
+        $('body').on('click', 'img', function () { location = "detailsFilms.html?id=" + this.id });
+        setInterval(function () { maFonction(); }, 5000);
     };
-    function chargePop() {
+    function chargePop() {     
+        for (var i = 0; i < 10; i++) {
 
-
-            //test xhr
-            var data = "{}";
-
-            var xhr = new XMLHttpRequest();
-            xhr.withCredentials = true;
-
-            xhr.addEventListener("readystatechange", function (result) {
-                if (this.readyState === this.DONE) {
-                    console.log("this.response xml=", this.responseXML);
-                    
+            $.ajax({
+                url: "https://api.themoviedb.org/3/movie/popular?api_key=85a1114cc9bcee8c748abdaaade8169a&language=en-US&page="+i,
+                cache: false,
+                type: "GET",
+                dataType: "json",
+                success: function (data) {  
+                    for (var i = 0; i < data.results.length; i++) {                        
+                        filmDbSave(data.results[i]);
+                    }
+                }
+                
+            });
+            $.ajax({
+                url: "https://api.themoviedb.org/3/person/popular?api_key=85a1114cc9bcee8c748abdaaade8169a&language=en-US&page=" + i,
+                cache: false,
+                type: "GET",
+                dataType: "json",
+                success: function (data) {
+                    for (var i = 0; i < data.results.length; i++) {
+                        actorDbSave(data.results[i]);
+                    }
                 }
             });
-
-            xhr.open("GET", "https://api.themoviedb.org/3/movie/popular?page=1&language=en-US&api_key=85a1114cc9bcee8c748abdaaade8169a");
-
-            xhr.send(data);
-            var test = xhr.responseXML;
-            console.log("le first child= ",test);
-            //$.ajax({
-            //    url: "https://api.themoviedb.org/3/movie/popular?api_key=85a1114cc9bcee8c748abdaaade8169a&language=en-US&page="+i,
-            //    cache: false,
-            //    type: "GET",
-            //    dataType: "json",
-            //    success: function (data) {  
-            //        console.log("les data:", data.results);
-                  
-            //        //console.log("data recu",data);
-            //        for (var i = 0; i < data.count; i++) {                        
-            //            listePop.push(data.results[i]);                        
-            //        }
-            //    },
-            //    complete: function (data) {
-            //        ////console.log("data recu", data);
-            //        //for (var i = 0; i < data.count; i++) {
-            //        //    listePop.push(data['results'][i]);
-            //        //};
-            //        //console.log("longueur complete: " + listePop.length);
-            //        //console.log("complete liste 0: "+listePop[0]);
-            //    }
-            //});
-
-            //$.ajax({
-            //    url: "https://api.themoviedb.org/3/person/popular?api_key=85a1114cc9bcee8c748abdaaade8169a&language=en-US&page=" + i,
-            //    cache: false,
-            //    type: "GET",
-            //    dataType: "json",
-            //    success: function (data) {
-            //        for (var i = 0; i < data['results'].length; i++) {
-            //            listeActeur.push(data.results[i]);
-            //        }
-            //    }
-            //});
-
- 
-    
-        console.log("la liste film contient: "+listePop.length , listePop);
-        console.log("la liste acteurs contient: ", listeActeur);
-        
+        }
     };
     function onPause() {
         // TODO: cette application a été suspendue. Enregistrez l'état de l'application ici.
@@ -100,100 +61,75 @@ function onSuccess(tx) {
 function onError(tx) {
     console.log("erreur creation tables" , tx.message);
 };
+
 function creerTableActeurs(tx) {
     tx.executeSql('DROP TABLE IF EXISTS actors');
-    tx.executeSql('CREATE TABLE IF NOT EXISTS actors(idActor unique ,name ,gender ,birth_date,birth_place ,death_day,popularity ,bio)');
-    tx.executeSql('INSERT INTO actors(idActor ,name, gender, birth_date, birth_place,death_day, popularity, bio) VALUES (1542, "moi", 0, 101/02/25, "quelque part", 21542/1/01, 1.2542, "Lorem ipsum dolor ");');
+    tx.executeSql('CREATE TABLE IF NOT EXISTS actors(idActor unique ,popularity,profile_path,name,known_for,adult,gender,birth_date,birth_place,death_day,bio)');
 
 }
+
 function creerTableFilms(tx) {
     tx.executeSql('DROP TABLE IF EXISTS films');
     tx.executeSql('DROP TABLE IF EXISTS company');
     tx.executeSql('CREATE TABLE IF NOT EXISTS films(idFilm unique,original_title,poster_path,adult,release_date,genre_ids,overview,title,backdrop_path,video)');
-    //tx.executeSql('INSERT INTO films(id,original_title,poster_path,adult,release_date,genre_ids,overview,title,backdrop_path,video) VALUES (121,"test_1","poster path",0,12/02/2014,125,"resume du film super geniaLl","titre numeo 2","le backdrop",0)');
 
 };
-function countFilms() {
-    var defer = $.Deferred();
-    var count = 0;
-    db.transaction(function (tx) {
-        tx.executeSql('SELECT idFilm,backdrop_path,original_title FROM films', [], function (tx, results) {
-            defer.resolve(results.rows.length);
-            return defer.promise();
-            listeFilm = results;
-            console.log("la liste des films : ",listeFilm);
-            //var len = results.rows.count;
-            //var i;
-            
-            //$img = $('img');
-            //for (i = 0; i < len; i++) {
-            //    //Set values coming from the database
-            //    //console.log("Id: " + results.rows.item(i).id +"\n"+
-            //    //    " titre: " + results.rows.item(i).title +"\n"+
-            //    //    " backdrop: " + results.rows.item(i).backdrop_path);
-            //    for (var j = 0; j < $('#film').length; j++) {
-            //        if ($('#film').attr('id')!==results.rows.item(i).idFilm) {
-            //            $("#film").append("<img class='col-12 col-sm-3 col-lg-2' id='" + results.rows.item(i).idFilm + "' src='https://image.tmdb.org/t/p/w500" + results.rows.item(i).backdrop_path + "' alt='" + results.rows.item(i).original_title + "' />")
 
-            //        }
-            //    }
-            //}
-        });
-    });
-}
-var countPromise = countFilms();
-$.when(countPromise).done(function (count) {
-    displayFilm(count);
-})
-function displayFilm(count) {
-    for (var i = 0; i < count; i++) {
-        $("#film").append("<img class='col-12 col-sm-3 col-lg-2' id='" + listFilm[i].idFilm + "' src='https://image.tmdb.org/t/p/w500" + results.rows.item(i).backdrop_path + "' alt='" + results.rows.item(i).original_title + "' />");
-    }
-}
-function insertActor(actor) {
-    //db.transaction(function insert(tx) {
-    
-        
-    //    var sql = 'INSERT INTO films(id,original_title,poster_path,adult,release_date,genre_ids,overview,title,backdrop_path,video) VALUES (?,?,?,?,?,?,?,?,?,?)';
-    //    tx.executeSql(sql, [leFilm['id'], leFilm['original_title'], leFilm['poster_path'], adult, leFilm['release_date'], leFilm['genre_ids'], leFilm['overview'], leFilm['title'], leFilm['backdrop_path'], video]);
-    //}, function errorCB(tx, err) {
-    //    console.log("Error processing SQL: " + err, tx.message);
-    //}, function successCB() {
-    //    //console.log("film " + leFilm['title'] + " inserted successfully");
+function filmDbSave(data) {
 
-    //});
-}
-function getLength(arr) {
-    return Object.keys(arr).length;
-}
-function save2Db() {
-    console.log("les liste film pour verif long=",listePop);
-    console.log("liste acteur pour verif long=" + getLength(listeActeur), listeActeur);
-    var listeCount = 0;
-    listePop.forEach(function () {
-        listeCount=listeCount+1;
-    });
-    for (var i = 0; i < listePop.length; i++) {
-        console.log("film -"+i+" titre:"+listePop[i]['title']);
-    }
-    console.log("le compteur renvoi: "+listeCount);
-    listePop.forEach(function (item) {
-        db.transaction(function insert(tx) {
+    db.transaction(function insert(tx) {
         var adult;
-        if (item['adult']) { adult = 1; } else adult = 0;
+        if (data.adult) { adult = 1; } else adult = 0;
         var video;
-        if (item['video']) { video = 1 } else video = 0;
+        if (data.video) { video = 1 } else video = 0;
         var sql = 'INSERT INTO films(idFilm,original_title,poster_path,adult,release_date,genre_ids,overview,title,backdrop_path,video) VALUES (?,?,?,?,?,?,?,?,?,?)';
-        tx.executeSql(sql, [item['id'], item['original_title'], item['poster_path'], adult, item['release_date'], item['genre_ids'], item['overview'], item['title'], item['backdrop_path'], video]);
-        }, function errorCB(tx, err) {
-            console.log("Error processing SQL: "+err , tx.message);
-        }, function successCB() {
-            //console.log("film "+leFilm['title']+" inserted successfully");
+        tx.executeSql(sql, [data.id, data.original_title, data.poster_path, adult, data.release_date,data.genre_ids, data.overview, data.title, data.backdrop_path, video]);
+    }, function errorCB(tx, err) {
+        console.log("Error processing SQL: "+err , tx.message);
+    }, function successCB() {
         
-        });
+    });
 
-    })
-    }
+}
     
+function actorDbSave(data) {
+    db.transaction(function insert(tx) {
+        var adult;
+        if (data.adult) { adult = 1; } else adult = 0;        
+        var sql = ' INSERT INTO actors(idActor,popularity,profile_path,name,known_for,adult,gender ,birth_date,birth_place ,death_day,bio) VALUES (?,?,?,?,?,?,?,?,?,?,?);';
+        tx.executeSql(sql, [data.id, data.popularity, data.profile_path, data.name, data.known_for, adult, "", "", "", "", ""]);
+    }, function errorCB(tx, err) {
+        console.log("Error processing SQL: " + err, tx.message);
+    }, function successCB() {
+        
+    });
 
-       
+}
+function maFonction() {
+    db.transaction(function (tx) {
+        //populate drop down for unites
+        tx.executeSql('SELECT * FROM films', [], function (tx, results) {
+            console.log("nouvelle fonction results", results);
+            var len = results.rows.length;
+            var i = 0;
+          
+            for (i = 0; i < len; i++) {
+                $('#film').append('<img class="col-12 col-s-6 col-sm-4 col-lg-2" id="'+results.rows[i].idFilm+'" src="https://image.tmdb.org/t/p/w500'+results.rows[i].backdrop_path+'" alt="Alternate Text" />')
+            }
+            document.getElementById("xhr").innerHTML = txt;
+        }, null);
+    });
+}
+getSelectedRow = function (val) {
+    var data = {};
+    db.transaction(function (tx) {
+        tx.executeSql('SELECT * FROM films', data, function (tx, results) {
+            var len = results.rows.length, i;
+            console.log("longueur row  " ,data);
+            for (i = 0; i < len; i++) {
+                alert(results.rows.item(i).log);
+            }
+        }, null);
+    });
+}
+
